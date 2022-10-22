2663c024d3aeb8803fabe7b27f0881d52e90bf91
mac <innakar@gmail.com>
2022-10-22 18:42:19 -0500

    add a lot

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,28 @@
 package jm.task.core.jdbc;
 
+import java.util.ArrayList;
+import java.util.List;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.*;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        List<User> users = new ArrayList<>();
+        users.add(new User("Marina", "Petrova", (byte) 35));
+        users.add(new User("Irina", "Petrova", (byte) 36));
+        users.add(new User("Anna", "Petrova", (byte) 37));
+        users.add(new User("Ivan", "Petrov", (byte) 38));
+
+        for (User us : users) {
+             userService.saveUser(us.getName(), us.getLastName(), (byte) us.getAge());
+             System.out.println("User с именем – " + us.getName() + " добавлен в базу данных");
+        }
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,32 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";// реализуйте настройку соединения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/mysql";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Innaya1308";
+
+    public Connection getConnection() {
+        Connection connection = null;
+
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Unable to load class.");
+            e.printStackTrace();
+        }
+
+        try {
+            connection = DriverManager.getConnection(Util.URL, Util.USER, Util.PASSWORD);
+            System.out.println("Connection OK!");
+        } catch (SQLException th) {
+            System.out.println("Connection ERROR!");
+            th.printStackTrace();
+        }
+
+        return connection;
+    }
+
 }

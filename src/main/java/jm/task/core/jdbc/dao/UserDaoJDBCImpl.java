b4d900b4ee1b873da68dbe3e74fe2694fb47a8f5
mac <innakar@gmail.com>
2022-10-23 16:51:39 -0500

    should be ok now

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -6,47 +6,32 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UserDaoJDBCImpl extends Util implements UserDao {
+public class UserDaoJDBCImpl implements UserDao {
+
+    private Connection connection = Util.getConnection();
     public UserDaoJDBCImpl() {
     }
-    @Override
+
     public void createUsersTable() {
-           String sql = "CREATE TABLE IF NOT EXISTS users(" +
-                   "ID BIGINT NOT NULL AUTO_INCREMENT, NAME VARCHAR(100), " +
-                   "LASTNAME VARCHAR(100), AGE INT, PRIMARY KEY (ID) )";
-
-           try (Connection connection = getConnection();
-                Statement stat = connection.createStatement()) {
-
-               stat.executeUpdate(sql);
-               System.out.println("Table was created!");
-
-           } catch (SQLException throwables) {
-               throwables.printStackTrace();
+        try (Statement stat = connection.createStatement()) {
+            stat.executeUpdate("CREATE TABLE IF NOT EXISTS users(" +
+                        "ID BIGINT NOT NULL AUTO_INCREMENT, NAME VARCHAR(100), " +
+                        "LASTNAME VARCHAR(100), AGE INT, PRIMARY KEY (ID) )");
+        } catch (SQLException e) {
+               e.printStackTrace();
            }
     }
 
-    @Override
     public void dropUsersTable() {
-        String sql = "DROP TABLE IF EXISTS users";
-
-        try (Connection connection = getConnection();
-             Statement stat = connection.createStatement()) {
-
-            stat.executeUpdate(sql);
-            System.out.println("Table was dropped");
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        try (Statement stat = connection.createStatement()) {
+            stat.executeUpdate("DROP TABLE IF EXISTS users");
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
-    @Override
     public void saveUser(String name, String lastName, byte age) {
-        String sql = "INSERT INTO users (NAME, LASTNAME, AGE) VALUES(?, ?, ?)";
-
-        try (Connection connection = getConnection();
-             PreparedStatement preStat = connection.prepareStatement(sql)) {
-
+        try (PreparedStatement preStat = connection.prepareStatement(
+                "INSERT INTO users (NAME, LASTNAME, AGE) VALUES(?, ?, ?)")) {
 
              preStat.setString(1, name);
              preStat.setString(2, lastName);
@@ -55,23 +40,19 @@ public class UserDaoJDBCImpl extends Util implements UserDao {
              preStat.executeUpdate();
              System.out.println("User was added!");
 
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
-    @Override
-    public void removeUserById(long id) {
-        String sql = "DELETE FROM users WHERE ID=?";
 
-        try (Connection connection = getConnection();
-             PreparedStatement preStat = connection.prepareStatement(sql)) {
+    public void removeUserById(long id) {
+        try (PreparedStatement preStat = connection.prepareStatement(
+                "DELETE FROM users WHERE ID=?")) {
 
             preStat.setLong(1, id);
-
             preStat.executeUpdate();
-            System.out.println("User was removed!");
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
     @Override
@@ -80,36 +61,28 @@ public class UserDaoJDBCImpl extends Util implements UserDao {
 
         String sql = "SELECT ID, NAME, LASTNAME, AGE FROM users";
 
-        try (Connection connection = getConnection();
-             Statement stat = connection.createStatement()) {
+        try (Statement stat = connection.createStatement();
+             ResultSet resultSet = stat.executeQuery(
+                     "SELECT ID, NAME, LASTNAME, AGE FROM users")) {
 
-            ResultSet resultSet = stat.executeQuery(sql);
             while (resultSet.next()) {
                 User user = new User();
                 user.setId(resultSet.getLong("ID"));
                 user.setName(resultSet.getString("NAME"));
                 user.setLastName(resultSet.getString("LASTNAME"));
                 user.setAge(resultSet.getByte("AGE"));
-
                 userList.add(user);
             }
-            System.out.println("List of users is ready!");
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
             }
         return userList;
     }
-    @Override
     public void cleanUsersTable() {
-        String sql = "DELETE FROM users";
-        try (Connection connection = getConnection();
-             Statement stat = connection.createStatement()) {
-
-            stat.executeUpdate(sql);
-            System.out.println("Table was cleaned!");
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        try (Statement stat = connection.createStatement()) {
+            stat.executeUpdate("DELETE FROM users");
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,20 +3,12 @@ package jm.task.core.jdbc.util;
 import java.sql.*;
 
 public class Util {
-    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";// реализуйте настройку соединения с БД
     private static final String URL = "jdbc:mysql://localhost:3306/mysql";
     private static final String USER = "root";
     private static final String PASSWORD = "Innaya1308";
 
-    public Connection getConnection() {
+    public static Connection getConnection() {
         Connection connection = null;
-
-        try {
-            Class.forName(JDBC_DRIVER);
-        } catch (ClassNotFoundException e) {
-            System.out.println("Unable to load class.");
-            e.printStackTrace();
-        }
 
         try {
             connection = DriverManager.getConnection(Util.URL, Util.USER, Util.PASSWORD);
